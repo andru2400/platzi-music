5ea9a1be19e495708362d3c647a4e79e1bd908cd
Andres David P <andru2400@hotmail.com>
2019-07-10 16:51:13 -0500

    Primer Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome de nuevo Andrés'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './scss/main.scss';
+</style>
