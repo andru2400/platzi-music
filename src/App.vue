13115abf0990e7a4a7b83e45f742ef2e75b43496
Andres David P <andru2400@hotmail.com>
2019-07-10 17:35:02 -0500

    N# 18

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
   <div id="app">
-      {{msg}}
+    <section class="section">
+      <nav class="nav has-shadow">
+        <div class="container">
+          <input type="text" class="input is-large" placeholder="Buscar Canciones" v-model="searchQuery">
+          <button class="button is-info is-large" @click="search">Buscar</button>
+          <button class="button is-danger is-large">&times;</button>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="columns">
+          <div class="cloumn" v-for="t in tracks" v-bind:key="t.name">
+            <h1 style="margin-top:50px">{{ t.name }} - {{ t.artist }}</h1>
+          </div>
+          <div v-text="searchMessage" style="margin-top:70px"></div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+
+ const tracks = [
+   {name: 'Musica ligera', artist: 'Soda'},
+   {name: 'Tren al sur', artist: 'Los p'},
+   {name: 'Te llore como a un rio', artist: 'Mana'}
+ ]
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome de nuevo Andrés'
+      searchQuery:"",
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
